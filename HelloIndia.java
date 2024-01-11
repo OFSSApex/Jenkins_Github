e8e4e248e4080e9a818aed843b7b9ef9f7224ba2
OFSSApex <107408219+OFSSApex@users.noreply.github.com>
2024-01-11 19:48:17 +0530

    Update HelloIndia.java

--- HelloIndia.java
+++ HelloIndia.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 		
 		System.out.println("Hello India............");
 		System.out.println("I Came to india");
-	System.out.println("commited in gisthub");
+	System.out.println("commited ins gisthub");
 	}
 }
